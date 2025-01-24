3e353b8745f1fb061f1d559b984628602366da10
Aryan100-crypto <aryankumarsanghi@gmail.com>
2025-01-24 14:56:46 +0530

    Solution to Logical NOT Operation

--- Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -1,15 +1,17 @@
 //|| or operator
 //&& and operator
 //! not operator
+//is used in braces before using logical operators like (!())
 #include<stdio.h>
 int main(){
     int a;
     scanf("%d",&a);
-    if(!(a>0)){
+    if a(!(a>0)){
         printf("True");
     }
     else{
         printf("False");
     }
+    
     return 0;
     }
