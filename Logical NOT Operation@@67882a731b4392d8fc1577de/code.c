abffcc29d531d1869dda42b6a665a46c35be9091
Aryan100-crypto <aryankumarsanghi@gmail.com>
2025-01-24 14:48:04 +0530

    Solution to Logical NOT Operation

---
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -0,0 +1,15 @@
+//|| or operator
+//&& and operator
+//! not operator
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a!>b){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+    return 0;
+    }
