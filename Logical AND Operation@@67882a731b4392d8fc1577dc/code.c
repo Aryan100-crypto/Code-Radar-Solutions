915022cb33c7c09abca92eb012b574531528e47b
Aryan100-crypto <aryankumarsanghi@gmail.com>
2025-01-24 14:41:03 +0530

    Solution to Logical AND Operation

--- Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -5,10 +5,10 @@ int main(){
     int a,b;
     scanf("%d%d",&a,&b);
     if(a>0||b>0){
-        print("True");
+        print("True\n");
     }
     else{
-        print("False");
+        print("False\n");
     }
     return 0;
 }
