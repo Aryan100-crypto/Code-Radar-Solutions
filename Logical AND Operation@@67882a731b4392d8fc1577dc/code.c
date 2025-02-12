5a97c12650155381aa50a5b5aa94d8a5c5651d70
Aryan100-crypto <aryankumarsanghi@gmail.com>
2025-02-12 18:16:55 +0530

    Solution to Logical AND Operation

--- Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -1,11 +1,10 @@
-//&& operator is the and operator
-//||operator is the or operator
 #include<stdio.h>
 int main(){
     int a,b;
     scanf("%d%d",&a,&b);
-    if(a>0&&b>0){
+    if((a&&b)>0){
         printf("True");
+
     }
     else{
         printf("False");
