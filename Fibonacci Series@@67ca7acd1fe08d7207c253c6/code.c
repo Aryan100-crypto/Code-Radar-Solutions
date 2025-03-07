4ce8da82176c8c0b32b620b95a90a8aa01942e3c
Aryan100-crypto <aryankumarsanghi@gmail.com>
2025-03-07 11:54:12 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,19 @@
+// Your code here...
+#include<stdio.h>
+int fib(int a);
+int main(){
+    int res;
+    res=fib(int a)
+    scanf("%d%d",&a,&res);
+    printf("%d",&res);
+    return 0;
+
+}
+int fib(int a){
+    if(k>0){
+        return a+fib(a-1);
+    }
+    else{
+        return 0;
+    }
+}
