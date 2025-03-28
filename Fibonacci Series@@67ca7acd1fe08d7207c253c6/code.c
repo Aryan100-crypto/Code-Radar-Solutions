aa239f58fc505dc74ed239edb7e9cd775ce1b8cf
Aryan100-crypto <aryankumarsanghi@gmail.com>
2025-03-28 15:26:25 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,25 +1,14 @@
 #include<stdio.h>
-
-int fib(int a);
-
-int main() {
-    int a, res;
-    
-    printf("Enter a number: ");
-    scanf("%d", &a);
-    
-    res = fib(a);
-    printf("Fibonacci of %d is: %d\n", a, res);
-    
-    return 0;
-}
-
-int fib(int a) {
-    if (a == 0) {
-        return 0; // Base case 1
-    } else if (a == 1) {
-        return 1; // Base case 2
-    } else {
-        return fib(a - 1) + fib(a - 2); // Recursive case
+int fib(int n){
+    if(n==0 || n==1){
+        return n;
     }
+    else{
+        return (n-1)+(n-2);
+    }
+}
+void main(){
+    int num;
+    scanf("%d",&num);
+    printf("%d",fib(num));
 }
